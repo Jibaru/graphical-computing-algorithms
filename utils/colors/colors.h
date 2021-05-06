20ac55f5c34e934dc2c69784cb8fe815b775aa9f
jibaru <xxnacho1997xx@gmail.com>
2021-05-06 11:38:02 -0500

    Created colors.h

---
+++ utils/colors/colors.h
@@ -0,0 +1,18 @@
+/**
+ * Colors
+ * 
+ * GLUT constant colors
+ */
+
+#ifndef COLORS_H
+#define COLORS_H
+
+#define GLUT_RED_COLOR 1.0f, 0.0f, 0.0f
+#define GLUT_GREEN_COLOR 0.0f, 1.0f, 0.0f
+#define GLUT_BLUE_COLOR 0.0f, 0.0f, 1.0f
+#define GLUT_YELLOW_COLOR 1.0f, 1.0f, 0.0f
+#define GLUT_MAGENTA_COLOR 1.0f, 0.0f, 1.0f
+#define GLUT_CYAN_COLOR 0.0f, 1.0f, 1.0f
+#define GLUT_WHITE_COLOR 1.0f, 1.0f, 1.0f
+
+#endif /* COLORS_H */
