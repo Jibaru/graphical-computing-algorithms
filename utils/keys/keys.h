d66aadb0a89928398b3696746f8ce63320bc9796
jibaru <xxnacho1997xx@gmail.com>
2021-05-20 15:10:21 -0500

    Created keys.h

---
+++ utils/keys/keys.h
@@ -0,0 +1,27 @@
+/**
+ * Keys
+ * 
+ * Key constants
+ */
+
+#ifndef KEYS_H
+#define KEYS_H
+
+#define KEY_LOWER_A 'a'
+#define KEY_LOWER_C 'c'
+#define KEY_LOWER_D 'd'
+#define KEY_LOWER_S 's'
+#define KEY_LOWER_W 'w'
+#define KEY_LOWER_Z 'z'
+
+#define KEY_UPPER_A 'A'
+#define KEY_UPPER_C 'C'
+#define KEY_UPPER_D 'D'
+#define KEY_UPPER_S 'S'
+#define KEY_UPPER_W 'W'
+#define KEY_UPPER_Z 'Z'
+
+#define KEY_PLUS '+'
+#define KEY_MINUS '-'
+
+#endif /* KEYS_H */
