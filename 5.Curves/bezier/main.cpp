04fe65d0192d6cf49341b24dced9c4526179bf48
jibaru <xxnacho1997xx@gmail.com>
2021-05-20 15:11:43 -0500

    Implemented Bezier curves

---
+++ 5.Curves/bezier/main.cpp
@@ -0,0 +1,89 @@
+/**
+ * Bezier Curve
+ * 
+ * Compile in root:
+ * g++ 5.Curves/bezier/main.cpp -lGL -lGLU -lglut
+ * 
+ * Compile in current folder:
+ * g++ main.cpp -lGL -lGLU -lglut
+ * 
+ */
+
+#include <vector>
+#include <GL/glut.h>
+
+#include "../../utils/colors/colors.h"
+#include "../../utils/points/point3d.h"
+
+#define T_START 0.0f
+#define T_END 1.0f
+#define T_INCREMENT 0.001f
+
+// R3 Points
+std::vector<Point3d> points = {
+    {0.5, 0.7, 1.0f},
+    {-0.5, 0.3, 0.9f},
+    {0.88, -0.7, 0.1f}};
+
+void draw()
+{
+    Point3d a, b, c;
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+
+    glPushMatrix();
+
+    // R3 Polylines
+    glBegin(GL_LINE_STRIP);
+    glColor3f(GLUT_GREEN_COLOR);
+    for (int i = 0; i < points.size(); i++)
+    {
+        glVertex3f(points[i].x, points[i].y, points[i].z);
+    }
+    glEnd();
+
+    // Bezier Curve
+    glBegin(GL_POINTS);
+    glColor3f(GLUT_YELLOW_COLOR);
+    for (float t = T_START; t <= T_END; t += T_INCREMENT)
+    {
+        a = (1 - t) * points[0] + t * points[1];
+        b = (1 - t) * points[1] + t * points[2];
+        c = (1 - t) * a + t * b;
+
+        glVertex3f(c.x, c.y, c.z);
+    }
+    glVertex3f(1.0f, 0.0f, 0.0f);
+    glEnd();
+
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+void init()
+{
+    glClearColor(GLUT_BLUE_COLOR, 1.0f);
+    glPointSize(2.0f);
+    glOrtho(800, 0, 600, 0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+int main(int argc, char *args[])
+{
+    glutInit(&argc, args);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(50, 50);
+    glutCreateWindow("Bezier Curve");
+
+    glutDisplayFunc(draw);
+
+    init();
+
+    glutMainLoop();
+
+    return 0;
+}
